4f9a628a2be1bbaff1c140e6bec4f8c39c11fed9
Jonathan DUFOUR <jodufour@jodufour.42.fr>
2022-10-21 15:39:16 -0400

    Setup mariadb container                               [DOING]   Setup database installation                         [DOING]     Use defaults-file instead of command-line options [DOING]

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+mariadb-install-db --defaults-file=/etc/my.cnf.d/mariadb-install-db.ini
+exit 0
+
 # Step 0
 # Check if the directory which will contain the mariadb server pid-file and the socket file exists, and if not, create it.
 if [ ! -d /run/mysqld ]; then
 	mkdir /run/mysqld
+	chown mysql:mysql /run/mysqld
 fi
 
 # Step 1
@@ -12,4 +16,4 @@ if [ ! -d /var/lib/mysql/mysql ]; then
 	
 fi
 
-exec mariadbd
+exec mariadbd --defaults-file=/etc/my.cnf.d/mariadbd.ini
